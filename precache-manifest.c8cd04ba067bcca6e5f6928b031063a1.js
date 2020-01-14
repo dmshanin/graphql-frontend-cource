30b94bff93e3fd8f6c141fcd245a632de1b216c7
Dmitriy Shanin <shanin_dv@shopandshow.ru>
2020-01-15 00:49:09 +0300

    Updates

---
+++ precache-manifest.c8cd04ba067bcca6e5f6928b031063a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc259cab21ae97f87a5968fbc529d8f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adc68825139a61ab22d1",
+    "url": "/static/js/2.bb059ad2.chunk.js"
+  },
+  {
+    "revision": "0c08a48b164499df78c0607b14eb2c76",
+    "url": "/static/js/2.bb059ad2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3a947d21bc4326bcfa84",
+    "url": "/static/js/main.f15d7daa.chunk.js"
+  },
+  {
+    "revision": "6366711d7bcfd761d712",
+    "url": "/static/js/runtime-main.e8848e60.js"
+  }
+]);
