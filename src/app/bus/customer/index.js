73d2e53c8b9626ed2eae4bb8f48b327f500bc706
Dmitriy Shanin <shanin_dv@shopandshow.ru>
2020-01-20 03:09:27 +0300

    lesson-5

--- src/app/bus/customer/index.js
+++ src/app/bus/customer/index.js
@@ -2,13 +2,25 @@
 import React from 'react';
 
 // Components
-import { List } from './list';
+import { useCustomer } from './hooks/useCustomer';
 
 export const Customer = () => {
-  return (
-    <div className='container p-5'>
-      <div className='h1 mb-4'>Customer</div>
-      <List />
-    </div>
+  const { handleChange, save, createdAccount } = useCustomer();
+
+  const customerJSX = createdAccount && (
+    <p>
+      We already created customer with name: { createdAccount.name }
+    </p>
   );
+
+  return (
+    <>
+      <h1>Registration</h1>
+      <input type="text" placeholder="name" name="name" onChange={handleChange} />
+      <input type="text" placeholder="username" name="username" onChange={handleChange} />
+      <input type="password" placeholder="password" name="password" onChange={handleChange} />
+      <button type="submit" onClick={save}>Save</button>
+      { customerJSX }
+    </>
+  )
 };
