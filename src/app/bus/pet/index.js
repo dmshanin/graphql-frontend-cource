e8c6534a1bc1f5536be8d9c07241cc7935cde931
shanin_dv <shanin_dv@shopandshow.ru>
2020-01-31 03:17:18 +0300

    lesson-6.2

--- src/app/bus/pet/index.js
+++ src/app/bus/pet/index.js
@@ -7,7 +7,7 @@ import { List } from './list';
 import { SpecialList } from './specialList';
 import { Profile } from './profile';
 import { CheckinPet } from './checkinPet';
-import { CheckoutPet } from './CheckoutPet';
+import { CheckoutPet } from './checkoutPet';
 
 export const Pet = () => {
   return (
