663ea43f2a61681889b8c75c93c980dba9205372
shanin_dv <shanin_dv@shopandshow.ru>
2020-01-31 03:27:39 +0300

    lesson-6.3

--- src/app/bus/pet/index.js
+++ src/app/bus/pet/index.js
@@ -2,12 +2,13 @@
 import React from 'react';
 
 // Components
-import { Counter } from './counter';
-import { List } from './list';
-import { SpecialList } from './specialList';
-import { Profile } from './profile';
+// import { Counter } from './counter';
+// import { List } from './list';
+// import { SpecialList } from './specialList';
+// import { Profile } from './profile';
 import { CheckinPet } from './checkinPet';
 import { CheckoutPet } from './checkoutPet';
+import { PetReturned } from './petReturned';
 
 export const Pet = () => {
   return (
@@ -15,6 +16,7 @@ export const Pet = () => {
       <h1>Pet</h1>
       <CheckinPet />
       <CheckoutPet />
+      <PetReturned />
       {/*<Profile />*/}
       {/*<Counter />*/}
       {/*<List />*/}
